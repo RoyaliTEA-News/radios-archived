54f22b362c2922aebc2d96947f2c1a50e9c7ea2c
Aideon <15018948+PiggyPlex@users.noreply.github.com>
2021-11-13 14:56:02 +0000

    Update upbeat.js

--- services/upbeat.js
+++ services/upbeat.js
@@ -1,4 +1,5 @@
-const genres = require('../genres')
+const genres = require('../genres'),
+      langs = require('langs')
 
 export default () => {
   return {
@@ -7,7 +8,7 @@ export default () => {
     "id": "f738f1f9-6de8-4b03-9889-c5b795ff6c10",
     "tagline": "UpBeat is a community powered online radio, providing 24/7 ad-free radio entertainment.",
     "genres": [genres.pop, genres.gaming, genres.community],
-    "language": "en_GB",
+    "language": langs.where('1', 'en'),
     "website": "https://upbeatradio.net",
     "sources": [{
       "name": "UpBeat",
