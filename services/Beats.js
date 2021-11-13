871d7bc408c06454880195b37c75406007eac6a5
jonathanjamandsimi <45129587+jonathanjamandsimi@users.noreply.github.com>
2021-11-13 18:57:51 +0000

    Create Beats.js

---
+++ services/Beats.js
@@ -0,0 +1,27 @@
+const tags = require('../tags'),
+      langs = require('langs')
+
+module.exports = () => {
+  return {
+    "name": "Beats",
+    "logo": "https://panel.itsbeats.net/avatars/defaultV1.jpg",
+    "id": "5f9004f0-c833-4849-bdce-1e23e010256b",
+    "tagline": "Beats is an online radio community that aims to provide the best online entertainment and the biggest beats from around the globe. Beats was first established back in 2019 with the return of it coming in 2021 with a fresh Ownership team ready to rock the radio community.",
+    "tags": [tags.pop, tags.gaming, tags.community, tags.news],
+    "language": langs.where('1', 'en'),
+    "website": "https://itsbeats.net",
+    "sources": [{
+      "name": "Beats",
+      "url": "https://radio.beats.xonos.gg/radio/8000/radio.mp3",
+      "default": true
+    }],
+    "api": {
+      "type": "custom",
+      "endpoint": "https://upbeatradio.net/api/v1/stats",
+      "presenter": "presenter.name",
+      "song": "stats.song.title",
+      "artist": "stats.song.artist",
+      "listeners": "stats.song.listeners"
+    }
+  }
+}
