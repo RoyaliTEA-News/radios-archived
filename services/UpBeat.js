ee214752bcd7d829c897610097aefff1b46da148
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2021-11-13 16:04:48 +0000

    Update UpBeat.js

--- services/UpBeat.js
+++ services/UpBeat.js
@@ -18,10 +18,10 @@ module.exports = () => {
     "api": {
       "type": "custom",
       "endpoint": "https://upbeatradio.net/api/v1/stats",
-      "presenter": "data.onair.name",
-      "song": "data.song.title",
-      "artist": "data.song.artist",
-      "listeners": "data.listeners"
+      "presenter": "onair.name",
+      "song": "song.title",
+      "artist": "song.artist",
+      "listeners": "listeners"
     }
   }
 }
