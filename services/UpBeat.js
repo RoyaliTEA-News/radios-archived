cd930019c9b65ceb56543c9fb31b1b6b0afe3b33
Aideon <15018948+PiggyPlex@users.noreply.github.com>
2021-11-13 21:47:52 +0000

    Update UpBeat.js

--- services/UpBeat.js
+++ services/UpBeat.js
@@ -5,13 +5,14 @@ module.exports = () => {
   return {
     "name": "UpBeat",
     "logo": "https://upbeatradio.net/staff/_assets/_avatarImages/default/default.png",
-    "aliases": ["ub"],
+    "aliases": ["UpBeat Radio", "UpBeatRadio", "ub"],
     "id": "f738f1f9-6de8-4b03-9889-c5b795ff6c10",
     "tagline": "UpBeat is a community powered online radio, providing 24/7 ad-free radio entertainment.",
     "tags": [tags.pop, tags.gaming, tags.community, tags.news],
     "language": langs.where('1', 'en'),
     "website": "https://upbeatradio.net",
     "discord": "https://discord.gg/tyc7qtm",
+    "premid": "UpBeatRadio",
     "sources": [{
       "name": "UpBeat",
       "url": "https://live.upbeat.pw",
