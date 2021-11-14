6e9c2049ebcc87315488b1c7dad1af0bf1810807
callumok2004 <callumokane123@gmail.com>
2021-11-14 03:13:49 +0000

    TruckersFM

---
+++ services/TruckersFM.js
@@ -0,0 +1,26 @@
+const tags = require('../tags'),
+  langs = require('langs')
+
+module.exports = () => {
+  return {
+    "name": "TruckersFM",
+    "logo": "https://truckersfm.s3.fr-par.scw.cloud/static/tfm-2020-adj.png",
+    "aliases": ["Truckers FM", "TFM"],
+    "id": "6a6079a1-198f-49b5-a989-cc1d011df4a9",
+    "tagline": "TruckersFM is a community radio station that is dedicated to the Truck Sim Community!",
+    "tags": [tags.simulation, tags.community, tags.pop, tags.gaming],
+    "language": langs.where('1', 'en'),
+    "website": "https://truckers.fm",
+    "discord": "https://discord.gg/truckersfm",
+    "premid": "TruckersFM",
+    "sources": [{
+      "name": "TruckersFM",
+      "url": "https://live.truckers.fm",
+      "default": true
+    }],
+    "api": {
+      "type": "azuracast",
+      "endpoint": "https://azura.truckers.fm/api/nowplaying/1"
+    }
+  }
+}
