40f3d5eab0f10b5c42681eda9ef7c56a1c4cd36c
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2021-11-14 03:51:19 +0000

    Update Beats.js

--- services/Beats.js
+++ services/Beats.js
@@ -5,7 +5,7 @@ module.exports = () => {
   return {
     "name": "Beats",
     "logo": "https://panel.itsbeats.net/avatars/defaultV1.jpg",
-    "id": "5f9004f0-c833-4849-bdce-1e23e010256b",
+    "id": "92d2483f-a99f-44ec-873e-214ac915aebe",
     "tagline": "An online radio community that aims to provide the best online entertainment.",
     "tags": [tags.pop, tags.community],
     "language": langs.where('1', 'en'),
