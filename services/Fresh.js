d35b830329238d994dd19d63ed263757214061f9
Aideon <15018948+PiggyPlex@users.noreply.github.com>
2021-11-15 08:03:39 +0000

    Update Fresh.js

--- services/Fresh.js
+++ services/Fresh.js
@@ -4,13 +4,13 @@ const tags = require('../tags'),
 module.exports = () => {
   return {
     "name": "Fresh",
-    "aliases": ["fresh", "freshradio", "fr"],
+    "aliases": ["freshradio"],
     "logo": "https://freshradio.pw/images/FRESH%20NEW.png",
     "id": "13d4a520-3329-4afb-9292-c9356c67b7cf",
     "tagline": "Fresh is a thriving internet station playing the latest and greatest hits.",
     "tags": [tags.pop, tags.community],
     "language": langs.where('1', 'en'),
-    "website": "https://freshradio.pw/",
+    "website": "https://freshradio.pw",
     "discord": "https://discord.gg/3BsaDyArsf",
     "sources": [{
       "name": "Fresh",
