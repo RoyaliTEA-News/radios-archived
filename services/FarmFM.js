9156f061caaae74c3172c33f9e375009c5f174b4
callumok2004 <callumokane123@gmail.com>
2021-11-16 22:50:48 +0000

    BigRigFM

--- services/FarmFM.js
+++ services/FarmFM.js
@@ -7,7 +7,7 @@ module.exports = () => {
     "logo": "https://cdn.discordapp.com/icons/829851298198716457/458bd39242ba908978d573f2504deec6.webp?size=512",
     "aliases": ["Farm FM", "farm", "ffm"],
     "id": "8d9c829f-1206-490e-a07f-dfd9fff0f6fd",
-    "tagline": "Playing the top hits and throwbacks 24/7 in Adelaide!",
+    "tagline": "Playing the latest hits, 24/7!",
     "tags": [tags.pop, tags.community, tags.simulation, tags.gaming],
     "language": langs.where('1', 'en'),
     "website": "https://farm-fm.com/",
